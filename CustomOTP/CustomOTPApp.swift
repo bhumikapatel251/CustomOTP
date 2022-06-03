c9d8deb7f9022bca24cd7e0e860225cbea4aa1be
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-06-03 16:35:10 +0530

    Initial Commit

---
+++ CustomOTP/CustomOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomOTPApp.swift
+//  CustomOTP
+//
+//  Created by Bhumika Patel on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
